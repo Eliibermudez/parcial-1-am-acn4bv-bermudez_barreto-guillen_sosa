77a7c3df547d89d0c1b90079bc01950b8c968d04
llubisayguillen-max <llubisay.guillen@davinci.edu.ar>
2026-06-30 12:49:18 -0300

    fix (parciales): creacion repository

--- app/src/main/java/com/example/app_campus/ParcialesRepository.java
+++ app/src/main/java/com/example/app_campus/ParcialesRepository.java
@@ -1,7 +1,10 @@
 package com.example.app_campus;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ParcialesRepository {
+
     public static List<Parcial> listaParciales = new ArrayList<>();
+
 }
